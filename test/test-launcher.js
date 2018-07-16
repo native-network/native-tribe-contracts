d657cebe83276f4172f121eee5bd44d087ab17fd
Alex Barkell <alexbarkell@gmail.com>
2018-07-16 15:55:56 -0600

    Refactoring  test slightly, including a failure test for bad values

--- test/test-launcher.js
+++ test/test-launcher.js
@@ -4,33 +4,53 @@ const Token = artifacts.require("SmartToken");
 
 contract('TribeLauncher', function () {
 
-  // TODO write all the tests
-  it("It should launch a new tribe contract", async function () {
-    
-    const sender = web3.eth.accounts[0]
-    const curator = web3.eth.accounts[0]
+  const sender = web3.eth.accounts[0]
+  const curator = web3.eth.accounts[0]
+  const nonCurator = web3.eth.accounts[1]
+  const _launchUuid = 123
+  let tokenInstance = null
+  let tribeLauncherInstance = null
+  let launchedTribeCount = null
+  let launchedTribeAddress = null
+  let launchedTribeInstance = null
+  let startingMembershipStatus = null
+  let amountRequiredForStaking = null
+  let stakedMembershipStatus = null
 
-    const tokenInstance = await Token.deployed()
-    
-    const tribeLauncherInstance = await TribeLauncher.deployed()
-    
-    await tribeLauncherInstance.launchTribe(123, 456, 789, curator, tokenInstance.address, {from: sender})
-
+  before(async () => {
+  
+  })
+  
+  beforeEach(async () => {
+    tokenInstance = await Token.deployed()
+    tribeLauncherInstance = await TribeLauncher.deployed()
+  })
+  it("It should launch a new tribe contract", async function () {      
+    const _minimumStakingRequirement = 10;
+    const _lockupPeriod = 0;
+    await tribeLauncherInstance.launchTribe(_launchUuid, _minimumStakingRequirement, _lockupPeriod, curator,
+                                            tokenInstance.address, {from: sender})
     const launchedTribeCount = await tribeLauncherInstance.launchedCount()
-
     const launchedTribeAddress = await tribeLauncherInstance.launchedTribes(launchedTribeCount - 1)
-    console.log('launchedTribeAddress', launchedTribeAddress)
-
     const launchedTribeInstance = await Tribe.at(launchedTribeAddress)
-
     const minimumStakingRequirement = await launchedTribeInstance.minimumStakingRequirement()
-    const lockupPeriod = await launchedTribeInstance.lockupPeriodSeconds()
-    
-    console.log('minimumStakingRequirement', minimumStakingRequirement)
-    console.log('lockupPeriod', lockupPeriod)
-    
-    assert(true)
+    // just a check to ensure we actually are getting data back and the contract is deployed
+    assert(minimumStakingRequirement.toString() === _minimumStakingRequirement.toString())
   })
 
+  it("It should fail to launch a tribe with a bad _minimumStakingRequirement", async function () {
+    const _minimumStakingRequirement = 'foo';
+    const _lockupPeriod = 0;
+
+    try {
+      await tribeLauncherInstance.launchTribe(_launchUuid, _minimumStakingRequirement, _lockupPeriod, curator,
+        tokenInstance.address, {from: sender})
+    } catch (e) {
+      assert(true)
+      return;
+    }
+    assert(false);
+    
+  })
 
 })
