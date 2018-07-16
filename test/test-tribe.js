17c4641e4d50d56270e166598b9d2d2fc6a39d68
Alex Barkell <alexbarkell@gmail.com>
2018-07-16 11:22:25 -0600

    add test for unstaking

--- test/test-tribe.js
+++ test/test-tribe.js
@@ -35,5 +35,45 @@ contract('Tribe', function () {
     assert(startingMembershipStatus === false && finalMembershipStatus === true)
   })
 
+  it("It should allow a user to unstake a tribe", async function () {
+    const sender = web3.eth.accounts[0]
+    const curator = web3.eth.accounts[0]
+
+    const tokenInstance = await Token.deployed()
+
+    const tribeLauncherInstance = await TribeLauncher.deployed()
+
+    const _launchUuid = 123;
+    const _minimumStakingRequirement = 456;
+    const _lockupPeriod = 0;
+
+    await tribeLauncherInstance.launchTribe(_launchUuid, _minimumStakingRequirement, _lockupPeriod, curator,
+                                            tokenInstance.address, {from: sender})
+
+    const launchedTribeCount = await tribeLauncherInstance.launchedCount()
+
+    const launchedTribeAddress = await tribeLauncherInstance.launchedTribes(launchedTribeCount - 1)
+
+    const launchedTribeInstance = await Tribe.at(launchedTribeAddress)
+
+    const startingMembershipStatus = await launchedTribeInstance.isMember(sender)
+    
+    const amountRequiredForStaking = await launchedTribeInstance.minimumStakingRequirement()
+    
+    await tokenInstance.approve(launchedTribeInstance.address, amountRequiredForStaking, {from: sender})
+
+    await launchedTribeInstance.stakeTribeTokens(amountRequiredForStaking, {from: sender})
+
+    const StakedMembershipStatus = await launchedTribeInstance.isMember(sender)
+    
+    assert(startingMembershipStatus === false && StakedMembershipStatus === true)
+
+    await launchedTribeInstance.unstakeTribeTokens(amountRequiredForStaking, {from: sender})
+    
+    const finalMembershipStatus = await launchedTribeInstance.isMember(sender)
+    
+    assert(finalMembershipStatus === false)
+
+  })
 
 })
