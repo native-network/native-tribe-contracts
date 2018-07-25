550157f3b3c91fafad570da96ccd8755d06b4804
Alex Barkell <alexbarkell@gmail.com>
2018-07-25 09:51:25 -0600

    Add test to enusre emit is fired

--- test/test-launcher.js
+++ test/test-launcher.js
@@ -33,10 +33,11 @@ contract('TribeLauncher', function () {
       tribeLauncherInstance = await TribeLauncher.deployed()
     })
 
-    // TODO test this to check for emitted event.  and all variable values
+    // TODO test this to check all variable values
     it("It should launch a new tribe contract", async function () {
       const _minimumStakingRequirement = 10;
       const _lockupPeriod = 0;
+
       await tribeLauncherInstance.launchTribe(
         _launchUuid,
         _minimumStakingRequirement,
@@ -54,7 +55,16 @@ contract('TribeLauncher', function () {
       const launchedTribeInstance = await Tribe.at(launchedTribeAddress)
       const minimumStakingRequirement = await launchedTribeInstance.minimumStakingRequirement()
       // just a check to ensure we actually are getting data back and the contract is deployed
-      assert(minimumStakingRequirement.toString() === _minimumStakingRequirement.toString())
+      
+      // wait for the event
+      tribeLauncherInstance.Launched((err, result) => {
+        if(err){
+          assert(false, err)
+        }
+
+        assert(result.args.launchUuid.toString() === _launchUuid.toString())
+        assert(minimumStakingRequirement.toString() === _minimumStakingRequirement.toString())  
+      })
     })
 
     it("It should fail to launch a tribe with a bad _minimumStakingRequirement", async function () {
