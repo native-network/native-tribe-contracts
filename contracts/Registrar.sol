6dbe5b089731d2691dfd017ebeb008ae7cc38140
seanluther <elmutt@gmail.com>
2018-08-02 12:35:36 -0600

    added contracts

---
+++ contracts/Registrar.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.8;
+
+import './utility/Owned.sol';
+import './Logger.sol';
+
+contract Registrar is Owned {
+    
+    address[] addresses;
+
+    Logger public logger;
+    
+    function addNewAddress(address _newAddress) public ownerOnly {
+        addresses.push(_newAddress);
+        logger.emitNewTribeAddress(_newAddress);
+    }
+
+    function getAddresses() public returns (address[]) {
+        return addresses;
+    }
+
+    constructor(address _address, address _loggerContractAddress) public {
+        logger = Logger(_loggerContractAddress);
+        addNewAddress(_address);
+    }
+}
